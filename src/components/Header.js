75e14a788ed6d0b0be78d4cd82f41920b1d064dd
mustafa altunok <mustafa.altunok@devbase.com.tr>
2022-05-23 09:00:16 +0300

    update fetch

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="header">
+      <img src="../troll-face.png" alt="troll-face" className="header--image" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Course - Project</h4>
+    </header>
+  );
+}
+
+export default Header;
